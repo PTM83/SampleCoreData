54cad0504fa62731ee5cba00443541a09ba42cb3
PTM83 <torres.m.pablo@gmail.com>
2024-09-03 01:43:19 -0400

    Initial Commit

---
+++ SampleCoreData/SampleCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCoreDataApp.swift
+//  SampleCoreData
+//
+//  Created by Pablo Andrés Torres Montes on 03-09-24.
+//
+
+import SwiftUI
+
+@main
+struct SampleCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
